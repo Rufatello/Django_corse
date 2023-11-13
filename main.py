92a2e7316db2e246899c38b517e5e6e260300ccf
wwwru <www.rufat@bk.ru>
2023-11-13 20:27:17 +0300

    start

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-<!doctype html>
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+# Для начала определим настройки запуска
+hostName = "localhost" # Адрес для доступа по сети
+serverPort = 8080 # Порт для доступа по сети
+
+class MyServer(BaseHTTPRequestHandler):
+
+    def __get_html_content(self):
+        return """
+        <!doctype html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -294,13 +304,13 @@
                     <form>
                         <div class="col-12">
                             <label for="exampleInputEmail1" class="form-label">Email адрес</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1"
+                            <input name= "email" type="email" class="form-control" id="exampleInputEmail1"
                                    aria-describedby="emailHelp">
                             <div id="emailHelp" class="form-text">Введите адрес электронной почты.</div>
                         </div>
                         <div class="col-12">
                             <label for="exampleInputPassword1" class="form-label">Пароль</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1">
+                            <input name = "password" type="password" class="form-control" id="exampleInputPassword1">
                         </div>
                         <div class="col-12 form-check">
 
@@ -372,3 +382,32 @@
 </script>
 </body>
 </html>
+        """
+
+
+    def do_GET(self):
+        """ Метод для обработки входящих GET-запросов """
+        query_components = parse_qs(urlparse(self.path).query)
+        print(query_components)
+        page_content = self.__get_html_content()
+        self.send_response(200) # Отправка кода ответа
+        self.send_header("Content-type", "text/html") # Отправка типа данных, который будет передаваться
+        self.end_headers() # Завершение формирования заголовков ответа
+        self.wfile.write(bytes(page_content, "utf-8")) # Тело ответа
+
+if __name__ == "__main__":
+    # Инициализация веб-сервера, который будет по заданным параметрам в сети
+    # принимать запросы и отправлять их на обработку специальному классу, который был описан выше
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        # Cтарт веб-сервера в бесконечном цикле прослушивания входящих запросов
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        # Корректный способ остановить сервер в консоли через сочетание клавиш Ctrl + C
+        pass
+
+    # Корректная остановка веб-сервера, чтобы он освободил адрес и порт в сети, которые занимал
+    webServer.server_close()
+    print("Server stopped.")
